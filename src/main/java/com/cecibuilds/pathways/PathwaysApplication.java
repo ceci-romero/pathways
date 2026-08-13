d36cf658087ba2bce43d251f39a1d8e07153f079
Ceci Romero <ceciliaromero2002@gmail.com>
2026-08-12 21:44:15 -0500

    Initial Spring Boot set up

---
+++ src/main/java/com/cecibuilds/pathways/PathwaysApplication.java
@@ -0,0 +1,13 @@
+package com.cecibuilds.pathways;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathwaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathwaysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cecibuilds/pathways/PathwaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cecibuilds.pathways;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathwaysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
